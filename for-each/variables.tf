db6ac52771fbfc1effd550b0201a7d580d9a3ed8
jai <jk8142727101@gmail.com>
2024-11-04 06:44:10 +0530

    terraform

---
+++ for-each/variables.tf
@@ -0,0 +1,24 @@
+variable "instance_names" {
+  type        = map
+  default     = {
+    db = "t3.small"
+    backend = "t3.micro"
+    frontend = "t3.micro"
+  }
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "Expense"
+        Terraform = "true"
+    }
+}
+
+variable "domain_name" {
+    default = "jai-awsdevops.online"
+}
+
+variable "zone_id" {
+    default = "Z056013810IR56E4ROFXQ"
+}
