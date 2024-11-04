15a233938d43e29b63b790796027686cadc6679b
jai <jk8142727101@gmail.com>
2024-11-04 06:14:45 +0530

    terraform

---
+++ remote-state/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "db" {
+
+    ami = "ami-09c813fb71547fc4f"
+    vpc_security_group_ids = ["sg-0ce45017043631db4"]
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "db"
+    }
+}
